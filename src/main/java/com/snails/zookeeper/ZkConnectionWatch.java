f7d85b224293970806a3920b8dfef60cdb7389a2
tsing-dong <ld320321@163.com>
2021-02-17 10:28:44 +0800

    first commit

---
+++ src/main/java/com/snails/zookeeper/ZkConnectionWatch.java
@@ -0,0 +1,13 @@
+package com.snails.zookeeper;
+
+import org.apache.zookeeper.WatchedEvent;
+import org.apache.zookeeper.Watcher;
+
+public class ZkConnectionWatch implements Watcher {
+
+    @Override
+    public void process(WatchedEvent watchedEvent) {
+
+    }
+
+}
